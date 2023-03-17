1d3b677cc6f9dd12e4ce5ead9969ba90760e19c1
Jose Carlos <josecdom94@gmail.com>
2023-03-17 23:12:02 +0100

    Get Endpoint

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Book = require('./models/bookModel');
 
 const app = express();
-
+const db = mongoose.connect('mongodb://root:example@localhost:27017/');
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Welcome to my Api');
-});
+bookRouter
+    .route('/books')
+    .get(async (req, res) => {
+        try {
+            const query = {};
+            if (req.query.genre) {
+                query.genre = req.query.genre;
+            }
+            const books = await Book.find(query);
+            return res.json(books);
+        } catch (err) {
+            return res.send(err);
+        }
+    });
 
+app.use('/api', bookRouter);
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
 });
